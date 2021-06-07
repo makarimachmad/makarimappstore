a4efbe50d8ff3ab43ddf67b093988f614e6f81b6
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-06-07 16:55:26 +0700

    gas kirim semua

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	
+	"gopsgheroku/config"
+	"gopsgheroku/handler/v1"
+	"gopsgheroku/middleware"
+	"gopsgheroku/repo"
+	"gopsgheroku/service"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB               = config.SetupKoneksi()
+	userRepo       repo.UserRepository    = repo.NewUserRepo(db)
+	productRepo    repo.ProductRepository = repo.NewProductRepo(db)
+	authService    service.AuthService    = service.NewAuthService(userRepo)
+	jwtService     service.JWTService     = service.NewJWTService()
+	userService    service.UserService    = service.NewUserService(userRepo)
+	productService service.ProductService = service.NewProductService(productRepo)
+	authHandler    v1.AuthHandler         = v1.NewAuthHandler(authService, jwtService, userService)
+	userHandler    v1.UserHandler         = v1.NewUserHandler(userService, jwtService)
+	productHandler v1.ProductHandler      = v1.NewProductHandler(productService, jwtService)
+)
+
+func main() {
+	defer config.CloseKoneksiDatabase(db)
+
+	fmt.Println(os.Getenv("DB_USER"))
+
+	server := gin.Default()
+
+	authRoutes := server.Group("api/auth")
+	{
+		authRoutes.POST("/login", authHandler.Login)
+		authRoutes.POST("/register", authHandler.Register)
+	}
+
+	userRoutes := server.Group("api/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userHandler.Profile)
+		userRoutes.PUT("/profile", userHandler.Update)
+	}
+
+	productRoutes := server.Group("api/product", middleware.AuthorizeJWT(jwtService))
+	{
+		productRoutes.GET("/", productHandler.All)
+		productRoutes.POST("/", productHandler.CreateProduct)
+		productRoutes.GET("/:id", productHandler.FindOneProductByID)
+		productRoutes.PUT("/:id", productHandler.UpdateProduct)
+		productRoutes.DELETE("/:id", productHandler.DeleteProduct)
+	}
+
+	checkRoutes := server.Group("api/check")
+	{
+		checkRoutes.GET("health", v1.Health)
+	}
+
+	server.Run()
+}
